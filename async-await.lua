3e9ca7cd12ad9df311d98d52b674903e30dfa778
TRtam <Diego.mta13@gmail.com>
2024-08-20 00:58:40 -0300

    Update async-await.lua

--- async-await.lua
+++ async-await.lua
@@ -1,182 +1,276 @@
-local locale = {
-    assert = assert,
-    isFunction = isFunction,
-    coroutine = coroutine,
-    table = table,
-    isTimer = isTimer,
-    killTimer = killTimer,
-    setTimer = setTimer,
-    math = math,
-    tonumber = tonumber,
-    dbQuery = dbQuery,
-    dbPoll = dbPoll,
-    passwordHash = passwordHash,
-    passwordVerify = passwordVerify,
-    fetchRemote = fetchRemote,
-}
+local thread_cache = {}
 
-async = {
-    threads = {},
+--- @class Thread
+--- @field co thread
+--- @field sleeping timer?
+--- @field awaiting Thread?
+--- @field values table?
+--- @field dead boolean?
+--- @field on_dead function?
+--- @field is_valid fun(self: Thread): boolean
+--- @field get_running_thread fun(): Thread?
+--- @field new fun(func: function, ...: any): Thread | false
+--- @field destroy fun(self: Thread): nil
+--- @field status fun(self: Thread): "dead" | "normal" | "running" | "suspended"
+--- @field pause fun(self: Thread): nil
+--- @field sleep fun(self: Thread, duration: number): nil
+--- @field await fun(self: Thread, other: Thread): any?
+--- @field resume fun(self: Thread, ...: any): any?
+local Thread = {}
+Thread.__index = Thread
 
-    new = function(fn)
-        if(not locale.isFunction(fn))then
-            return false
-        end
-    
-        return function(...)
-            local thread = {co = locale.coroutine.create(fn)}
-    
-            locale.assert(thread.co, "async.new() - failed to create locale.coroutine")
-        
-            async.threads[thread.co] = thread
-        
-            function thread:status()
-                return locale.coroutine.status(self.co)
-            end
-
-            function thread:pause()
-                local runningThread = async.threads[locale.coroutine.running()]
-    
-                locale.assert(runningThread, "async.thread:pause() - this function can only be called from an async thread")
-    
-                return locale.coroutine.yield()
-            end
-    
-            function thread:resume(...)
-                if(locale.isTimer(self.sleeping) or self.awaiting)then
-                    return false
-                end
-
-                local values
-
-                if(thread:status() == "suspended")then
-                    values = {locale.coroutine.resume(self.co, ...)}
-
-                    if(thread:status() == "dead")then
-                        local success = locale.table.remove(values, 1)
-                        
-                        if(not success)then
-                            thread.values = {false, unpack(values)}
-                        else
-                            thread.values = values
-                        end
-                    end
-                end
-
-                return locale.table.unpack(values)
-            end
-
-            thread:resume(...)
-
-            return thread
-        end
-    end
-}
-
-function sleep(ms)
-    local runningThread = async.threads[locale.coroutine.running()]
-
-    locale.assert(runningThread, "async.sleep() - this function can only be called from an async thread")
-
-    ms = locale.tonumber(ms) or 1
-
-    runningThread.sleeping = setTimer(
-        function()
-            runningThread.sleeping = nil
-            runningThread:resume()
-        end,
-        ms,
-        1
-    )
-
-    return runningThread:pause()
+function Thread.is_valid(self)
+	return getmetatable(self) == Thread
 end
 
-await = {
-    dbQuery = function(connection, queryString, ...)
-        local runningThread = async.threads[locale.coroutine.running()]
+function Thread.get_running_thread()
+	return thread_cache[coroutine.running()]
+end
 
-        locale.assert(runningThread, "async.await.dbQuery() - this function can only be called from an async thread")
-    
-        local waiting = locale.dbQuery(function(query) return runningThread:resume(locale.dbPoll(query, 0)) end, connection, queryString, ...)
-    
-        if(waiting)then return runningThread:pause() else return false end
-    end,
+function Thread.new(func, ...)
+	if type(func) ~= "function" then
+		return false
+	end
+	local thread = setmetatable({ co = coroutine.create(func) }, Thread)
+	thread_cache[thread.co] = thread
+	thread:resume(...)
+	return thread
+end
 
-    passwordHash = function(password, algorithm, options)
-        local runningThread = async.threads[locale.coroutine.running()]
+function Thread:destroy()
+	thread_cache[self.co] = nil
+	setmetatable(self, nil)
+end
 
-        locale.assert(runningThread, "async.await.passwordHash() - this function can only be called from an async thread")
-            
-        local waiting = locale.passwordHash(password, algorithm, options or {}, function(...) return runningThread:resume(...) end)
-    
-        if(waiting)then return runningThread:pause() else return false end
-    end,
+function Thread:status()
+	return coroutine.status(self.co)
+end
 
-    passwordVerify = function(password, hash, options)
-        local runningThread = async.threads[locale.coroutine.running()]
+function Thread:pause()
+	return coroutine.yield()
+end
 
-        locale.assert(runningThread, "async.await.passwordVerify() - this function can only be called from an async thread")
-    
-        local waiting = locale.passwordVerify(password, hash, options or {}, function(...) return runningThread:resume(...) end)
-    
-        if(waiting)then return runningThread:pause() else return false end
-    end,
+function Thread:sleep(duration)
+	if type(duration) ~= "number" then
+		return false
+	end
+	self.sleeping = setTimer(function()
+		self.sleeping = nil
+		self:resume()
+	end, duration, 1)
+	return self:pause()
+end
 
-    fetchRemote = function(url, options)
-        local runningThread = async.threads[locale.coroutine.running()]
+function Thread:await(other)
+	if not Thread.is_valid(other) then
+		return false
+	end
+	if other == self then
+		return false
+	end
+	if other.dead and other.values then
+		return unpack(other.values)
+	end
+	self.awaiting = other
+	function other.on_dead()
+		self.awaiting = nil
+		self:resume(unpack(other.values))
+	end
+	return self:pause()
+end
 
-        locale.assert(runningThread, "async.await.fetchRemote() - this function can only be called from an async thread")
-            
-        locale.fetchRemote(url, options, function(...) return runningThread:resume(...) end)
-    
-        return runningThread:pause()
-    end,
+function Thread:resume(...)
+	if self.sleeping then
+		return
+	end
+	if self.awaiting then
+		return
+	end
+	if self:status() == "dead" then
+		return unpack(self.values)
+	end
+	if self:status() == "suspended" then
+		local values = { coroutine.resume(self.co, ...) }
+		table.remove(values, 1)
+		self.values = values
+	end
+	if self:status() == "dead" then
+		self.dead = true
+		if self.on_dead then
+			self.on_dead()
+		end
+		thread_cache[self.co] = nil
+	end
+	return unpack(self.values)
+end
 
-    new = function(thread)
-        local runningThread = async.threads[locale.coroutine.running()]
+--- @param func function
+--- @return function | false
+function Async(func)
+	if type(func) ~= "function" then
+		return false
+	end
+	return function(...)
+		return Thread.new(func, ...)
+	end
+end
 
-        locale.assert(runningThread, "async.await.new() - this function can only be called from an async thread")
+--- @param thread Thread
+--- @return any
+function Await(thread)
+	if not Thread.is_valid(thread) then
+		return false
+	end
+	local running_thread = Thread.get_running_thread()
+	if not running_thread then
+		return false
+	end
+	return running_thread:await(thread)
+end
 
-        if((not locale.table.valid(thread)) or (not async.threads[thread.co]) or (runningThread.co == thread.co))then
-            return false
-        end
+--- @param duration number
+--- @return any
+function Sleep(duration)
+	if type(duration) ~= "number" then
+		return false
+	end
+	local running_thread = Thread.get_running_thread()
+	if not running_thread then
+		return false
+	end
+	return running_thread:sleep(duration)
+end
 
-        if(thread.values)then
-            return locale.table.unpack(thread.values)
-        end
+local _dbQuery = dbQuery
 
-        runningThread.awaiting = locale.setTimer(
-            function()
-                if(thread.values)then
-                    locale.killTimer(runningThread.awaiting)
+dbQuery = Async(function(...)
+	if not _dbQuery then
+		return false
+	end
+	local running_thread = Thread.get_running_thread()
+	if not running_thread then
+		return false
+	end
+	local result = _dbQuery(function(query)
+		running_thread:resume(dbPoll(query, 0))
+	end, ...)
+	if not result then
+		return false
+	end
+	return running_thread:pause()
+end)
 
-                    runningThread.awaiting = nil
-                    runningThread:resume(locale.table.unpack(thread.values))
-                end
-            end,
-            50,
-            0
-        )
+local _passwordHash = passwordHash
 
-        return runningThread:pause()
-    end,
-}
+passwordHash = Async(function(password, algorithm, options)
+	if not _passwordHash then
+		return false
+	end
+	local running_thread = Thread.get_running_thread()
+	if not running_thread then
+		return false
+	end
+	local result = _passwordHash(password, algorithm, options or {}, function(...)
+		running_thread:resume(...)
+	end)
+	if not result then
+		return false
+	end
+	return running_thread:pause()
+end)
 
-setmetatable(
-    async, 
-    {
-        __call = function(self, ...)
-            return self.new(...)
-        end
-    }
-)
+local _passwordVerify = passwordVerify
 
-setmetatable(
-    await,
-    {
-        __call = function(self, ...)
-            return self.new(...)
-        end
-    }
-)
+passwordVerify = Async(function(password, hash, options)
+	if not _passwordVerify then
+		return false
+	end
+	local running_thread = Thread.get_running_thread()
+	if not running_thread then
+		return false
+	end
+	local result = _passwordVerify(password, hash, options or {}, function(...)
+		running_thread:resume(...)
+	end)
+	if not result then
+		return false
+	end
+	return running_thread:pause()
+end)
+
+local _fetchRemote = fetchRemote
+
+fetchRemote = Async(function(url, options)
+	if not _fetchRemote then
+		return false
+	end
+	local running_thread = Thread.get_running_thread()
+	if not running_thread then
+		return false
+	end
+	local result = _fetchRemote(url, options or {}, function(...)
+		running_thread:resume(...)
+	end)
+	if not result then
+		return false
+	end
+	return running_thread:pause()
+end)
+
+local _encodeString = encodeString
+
+encodeString = Async(function(algorithm, input, options)
+	if not _encodeString then
+		return false
+	end
+	local running_thread = Thread.get_running_thread()
+	if not running_thread then
+		return false
+	end
+	local result = _encodeString(algorithm, input, options or {}, function(...)
+		running_thread:resume(...)
+	end)
+	if not result then
+		return false
+	end
+	return running_thread:pause()
+end)
+
+local _decodeString = decodeString
+
+decodeString = Async(function(algorithm, input, options)
+	if not _decodeString then
+		return false
+	end
+	local running_thread = Thread.get_running_thread()
+	if not running_thread then
+		return false
+	end
+	local result = _decodeString(algorithm, input, options or {}, function(...)
+		running_thread:resume(...)
+	end)
+	if not result then
+		return false
+	end
+	return running_thread:pause()
+end)
+
+local _generateKeyPair = generateKeyPair
+
+---@diagnostic disable-next-line: lowercase-global
+generateKeyPair = Async(function(algorithm, options)
+	if not _generateKeyPair then
+		return false
+	end
+	local running_thread = Thread.get_running_thread()
+	if not running_thread then
+		return false
+	end
+	local result = _generateKeyPair(algorithm, options or {}, function(...)
+		running_thread:resume(...)
+	end)
+	if not result then
+		return false
+	end
+	return running_thread:pause()
+end)
